d22cd57e4c5700df53ddb6bebc65f6d3d095774d
Paula Feleggakis <paula@curvetomorrow.com.au>
2017-02-24 11:51:37 +1100

    comment out Tulip as an avaialable product - due to pending spec

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,8 @@
 
 rose = Product.create(name: 'Rose', code: 'R12')
 lily = Product.create(name: 'Lily', code: 'L09')
-tulip = Product.create(name: 'Tulip', code: 'T58')
+# tulip = Product.create(name: 'Tulip', code: 'T58')
 
 rose.bundles = [Bundle.create(bundle_quantity: 5, bundle_cost: 6.99), Bundle.create(bundle_quantity: 10, bundle_cost: 12.99)]
 lily.bundles= [Bundle.create(bundle_quantity: 3, bundle_cost: 9.95), Bundle.create(bundle_quantity: 6, bundle_cost: 16.95), Bundle.create(bundle_quantity: 9, bundle_cost: 24.95)]
-tulip.bundles = [Bundle.create(bundle_quantity: 3, bundle_cost: 5.95), Bundle.create(bundle_quantity: 5, bundle_cost: 9.95), Bundle.create(bundle_quantity: 9, bundle_cost: 16.99)]
+# tulip.bundles = [Bundle.create(bundle_quantity: 3, bundle_cost: 5.95), Bundle.create(bundle_quantity: 5, bundle_cost: 9.95), Bundle.create(bundle_quantity: 9, bundle_cost: 16.99)]
